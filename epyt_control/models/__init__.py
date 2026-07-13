b3e574872ae8e879576f3302323ca37485a35dbe
André Artelt <aartelt@techfak.de>
2026-07-13 09:52:32 +0200

    Make PyTorch device available to the user

--- epyt_control/models/__init__.py
+++ epyt_control/models/__init__.py
@@ -1,5 +1,5 @@
 try:
-    from .pi_gnn_hydsurrogate import PIGNNModel
+    from .pi_gnn_hydsurrogate import PIGNNModel, device
 except:
     print("Failed to import 'PIGNNModel'.")
     print("You probably did not correctly install the optional extension [hydsurrogate] " +
